d6f9819dfad7880b1716d836340e97fcbae097a8
Anshu Kumar Singh <thisissidam@gmail.com>
2025-02-05 12:33:37 +0530

    Replace GNav with BottomNavigationBar

--- lib/shared/widgets/bottom_nav/bottom_nav_bar.dart
+++ lib/shared/widgets/bottom_nav/bottom_nav_bar.dart
@@ -1,41 +1,39 @@
 import 'package:Rem/feature/archives/presentation/screens/archive_screen.dart';
 import 'package:Rem/feature/home/presentation/screens/home_screen.dart';
 import 'package:Rem/feature/settings/presentation/screens/settings_screen.dart';
+import 'package:Rem/shared/utils/logger/global_logger.dart';
 import 'package:Rem/shared/widgets/snack_bar/custom_snack_bar.dart';
 import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../utils/logger/global_logger.dart';
-
-class NavigationLayer extends StatefulWidget {
+@immutable
+class NavigationLayer extends HookConsumerWidget {
   const NavigationLayer({super.key});
 
-  @override
-  State<NavigationLayer> createState() => _NavigationLayerState();
-}
-
-class _NavigationLayerState extends State<NavigationLayer> {
-  final List<GButton> _tabs = [
-    GButton(icon: Icons.archive, text: "Archive"),
-    GButton(icon: Icons.home, text: "Home"),
-    GButton(icon: Icons.settings, text: "Settings"),
-  ];
-
-  final List<Widget> _pages = [
-    ArchiveScreen(),
-    HomeScreen(),
-    SettingsScreen(),
-  ];
-
-  int _selectedTab = 1; // Homepage by default.
+  Widget _currentScreen(int index) {
+    switch (index) {
+      case 0:
+        return ArchiveScreen();
+      case 1:
+        return const HomeScreen();
+      case 2:
+        return SettingsScreen();
+      default:
+        return const HomeScreen();
+    }
+  }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final ValueNotifier<int> currentScreen = useState<int>(1);
     gLogger.i('Build navigation layer');
     return Scaffold(
       body: DoubleBackToCloseApp(
-        child: _pages[_selectedTab],
+        child: AnimatedSwitcher(
+            duration: Duration(milliseconds: 300),
+            child: _currentScreen(currentScreen.value)),
         snackBar: buildCustomSnackBar(
           content: Align(
             alignment: Alignment.center,
@@ -44,16 +42,26 @@ class _NavigationLayerState extends State<NavigationLayer> {
           ),
         ),
       ),
-      bottomNavigationBar: GNav(
-        tabs: _tabs,
-        gap: 8,
-        selectedIndex: _selectedTab,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        onTabChange: (index) {
-          gLogger.i('Navigating to ${_tabs[index].text}');
-          setState(() {
-            _selectedTab = index;
-          });
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.archive),
+            label: "Archive",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+        unselectedFontSize: 12,
+        selectedFontSize: 14,
+        currentIndex: currentScreen.value,
+        onTap: (index) {
+          currentScreen.value = index;
         },
       ),
     );
