d2ec0498d465c17794997d8002ee3087a7b433ad
Anshu Kumar Singh <thisissidam@gmail.com>
2025-03-19 01:49:48 +0530

    Enhance Whats New dialog to display new features and improvements; update version check logic to use build number for notifications

--- lib/shared/utils/misc_methods.dart
+++ lib/shared/utils/misc_methods.dart
@@ -19,33 +19,4 @@ class MiscMethods {
     }
     await Future<void>.delayed(const Duration(seconds: 1));
   }
-
-  static int compareVersions(String newV, String oldV) {
-    final RegExp versionRegex = RegExp(r'^v?(\d+)\.(\d+)\.(\d+)(?:\+(\d+))?$');
-
-    List<int> parseVersion(String version) {
-      final RegExpMatch? match = versionRegex.firstMatch(version);
-      if (match == null) {
-        throw FormatException('Invalid version format: $version');
-      }
-
-      return <int>[
-        int.parse(match.group(1) ?? '0'), // Major
-        int.parse(match.group(2) ?? '0'), // Minor
-        int.parse(match.group(3) ?? '0'), // Patch
-        int.parse(match.group(4) ?? '0'), // Build metadata
-      ];
-    }
-
-    final List<int> v1Parts = parseVersion(newV);
-    final List<int> v2Parts = parseVersion(oldV);
-
-    // Compare major, minor, patch, and build metadata
-    for (int i = 0; i < v1Parts.length; i++) {
-      if (v1Parts[i] > v2Parts[i]) return 1;
-      if (v1Parts[i] < v2Parts[i]) return -1;
-    }
-
-    return 0;
-  }
 }
--- lib/shared/widgets/whats_new_dialog/whats_new_dialog.dart
+++ lib/shared/widgets/whats_new_dialog/whats_new_dialog.dart
@@ -1,124 +1,65 @@
-// // ignore_for_file: lines_longer_than_80_chars
-
-// import 'package:flutter/material.dart';
-
-// import '../../utils/logger/global_logger.dart';
-
-// class WhatsNewDialog extends StatelessWidget {
-//   const WhatsNewDialog({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     gLogger.i("Showing what's new dialog");
-//     return PopScope(
-//       canPop: false,
-//       child: AlertDialog(
-//         backgroundColor: Theme.of(context).cardColor,
-//         title: ListTile(
-//           leading: const Icon(Icons.new_releases_outlined),
-//           title: Text(
-//             "What's New",
-//             style: Theme.of(context).textTheme.titleLarge,
-//           ),
-//           trailing: IconButton(
-//             icon: const Icon(Icons.close),
-//             onPressed: () {
-//               Navigator.of(context).pop();
-//             },
-//           ),
-//         ),
-//         insetPadding: const EdgeInsets.symmetric(horizontal: 32),
-//         contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-//         content: SingleChildScrollView(
-//           child: Column(
-//             mainAxisSize: MainAxisSize.min,
-//             children: getWhatsNewTileContent(context),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
 // ignore_for_file: lines_longer_than_80_chars
 
-//   List<Widget> getWhatsNewTileContent(BuildContext context) {
-//     return <Widget>[
-//       Padding(
-//         padding: const EdgeInsets.all(16),
-//         child: DecoratedBox(
-//           decoration: BoxDecoration(
-//             borderRadius: BorderRadius.circular(12),
-//             color: Theme.of(context).colorScheme.errorContainer,
-//           ),
-//           child: ListTile(
-//             title: Row(
-//               children: <Widget>[
-//                 const Icon(
-//                   Icons.warning,
-//                   size: 16,
-//                 ),
-//                 const SizedBox(
-//                   width: 8,
-//                 ),
-//                 Text(
-//                   'Important!',
-//                   style: Theme.of(context).textTheme.titleMedium!.copyWith(
-//                         color: Theme.of(context).colorScheme.onErrorContainer,
-//                       ),
-//                 ),
-//               ],
-//             ),
-//             subtitle: Text(
-//               'Previously, the app used a background service to show you repeated'
-//               " notifications. This has been changed, and now the app uses the android's"
-//               ' Alarms and Reminders feature. \n\nHence it is recommended that you'
-//               " reinstall the app. This would remove the background service's residuals left"
-//               ' out even after the update.\n\nAn experimental backup and restore option is present'
-//               ' in the settings. You can backup your reminders with it if necessary and restore them '
-//               ' after reinstalling.',
-//               style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-//                     color: Theme.of(context).colorScheme.onErrorContainer,
-//                   ),
-//             ),
-//           ),
-//         ),
-//       ),
-//       Padding(
-//         padding: const EdgeInsets.all(8),
-//         child: DecoratedBox(
-//           decoration: BoxDecoration(
-//             borderRadius: BorderRadius.circular(12),
-//             color: Theme.of(context).cardColor,
-//           ),
-//           child: ListTile(
-//             title: Row(
-//               children: <Widget>[
-//                 const Icon(
-//                   Icons.new_releases,
-//                   size: 16,
-//                 ),
-//                 const SizedBox(
-//                   width: 8,
-//                 ),
-//                 Text(
-//                   'New Stuff',
-//                   style: Theme.of(context).textTheme.titleMedium,
-//                 ),
-//               ],
-//             ),
-//             subtitle: Text(
-//               "- Use of Android's Alarms and Reminders feature for showing repeat notifications\n"
-//               '- Swipe Actions\n'
-//               '- Text Scaling\n'
-//               '- Backup and restore\n'
-//               '- Improvements to Title Parsing\n'
-//               '- Auto-apply parsed title from next times\n'
-//               '- Various minor changes',
-//               style: Theme.of(context).textTheme.bodyMedium,
-//             ),
-//           ),
-//         ),
-//       ),
-//     ];
-//   }
-// }
+import 'package:flutter/material.dart';
+
+import '../../utils/logger/global_logger.dart';
+
+class WhatsNewDialog extends StatelessWidget {
+  const WhatsNewDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    gLogger.i("Showing what's new dialog");
+    return PopScope(
+      canPop: false,
+      child: AlertDialog(
+        backgroundColor: Theme.of(context).cardColor,
+        title: ListTile(
+          leading: const Icon(Icons.new_releases_outlined),
+          title: Text(
+            "What's New",
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.close),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        insetPadding: const EdgeInsets.symmetric(horizontal: 32),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+        content: const SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.music_note, size: 12),
+                title: Text(
+                  'Added separate notification tone. Easy to differentiate notification through tone now.',
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.circle, size: 12),
+                title: Text('Backup/Restore are now functional.'),
+              ),
+              ListTile(
+                leading: Icon(Icons.circle, size: 12),
+                title: Text(
+                  'Added postpone button for no rush reminders in reminder sheet.',
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.circle, size: 12),
+                title: Text(
+                  'Fixed issue: Save button in reminder sheet for no rush reminder saved with new date.',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
